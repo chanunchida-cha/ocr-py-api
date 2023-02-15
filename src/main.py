fb893f60c0a02e177e83ae90bdbe900d41953ea8
Yok <cc.chanunn@gmail.com>
2023-02-15 13:40:55 +0700

    edit host and port uvicorn

--- src/main.py
+++ src/main.py
@@ -67,4 +67,4 @@ async def create_upload_file(uploaded_file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
